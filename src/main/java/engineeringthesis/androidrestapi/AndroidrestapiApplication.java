90c51cba74217a8b9da39a887490f32e483764a1
Adam Zimny <adamczyk97@op.pl>
2020-06-27 17:59:13 +0200

    my first repository

---
+++ src/main/java/engineeringthesis/androidrestapi/AndroidrestapiApplication.java
@@ -0,0 +1,13 @@
+package engineeringthesis.androidrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/engineeringthesis/androidrestapi/AndroidrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package engineeringthesis.androidrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
