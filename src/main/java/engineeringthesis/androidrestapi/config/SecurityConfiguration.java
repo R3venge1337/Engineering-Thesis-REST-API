d070c307e8cc02be786d24983e6e7f4f1bc31ddc
Adam Zimny <adamczyk97@op.pl>
2021-02-17 16:16:10 +0100

    Spring security add new antMatchers

--- src/main/java/engineeringthesis/androidrestapi/config/SecurityConfiguration.java
+++ src/main/java/engineeringthesis/androidrestapi/config/SecurityConfiguration.java
@@ -52,23 +52,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
 				.authorizeRequests()
-				.antMatchers(HttpMethod.GET,"/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**","/api/login","/api/accounts/**","/api/accounts","/api/roles","/api/roles/**","/api/teachers","/api/teachers/**"
+				.antMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**","/api/login","/api/accounts/**","/api/accounts","/api/roles","/api/roles/**","/api/teachers","/api/teachers/**"
 						,"/api/children","/api/children/**","/api/languages","/api/languages/**","/api/games","/api/games/**","/api/categories","/api/categories/**"
 						,"/api/words","/api/words/**","/api/images","/api/images/**"
 						,"/api/audio","/api/audio/**","/api/authenticate","/api/teachers/accounts"
-						,"/api/children/accounts","/api/children/accounts/**","/api/teachers/accounts/**"
+						,"/api/children/accounts","/api/children/accounts/**","/api/teachers/accounts/**","/api/token","/api/authenticate"
 						,"/api/statistics","/api/statistics/**","/api/matches","/api/matches/**","/api/statistics/statisticResults","/api/statistics/statisticResults/**","/api/results","/api/results/**").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/token").hasAnyRole("nauczyciel","administrator","dziecko")
-				.antMatchers(HttpMethod.PUT,"/api/accounts/{accountId}","/api/words/{audioId}").hasAnyRole("nauczyciel","administrator","dziecko")
-				.antMatchers(HttpMethod.PUT,"/api/audio/{audioId}","/api/categories/{categoryId}","/api/games/{gameId}","/api/matches/{gameplayId}","/api/results/{gameplayId}"
-						,"/api/images/{imageId}","/api/languages/{languageId}","/api/statistics/statisticResults/{statisticResultsId}","/api/statistics/{statisticId}","/api/teachers/{teacherId}","/api/words/{wordId}").hasAnyRole("nauczyciel","administrator")
-				.antMatchers(HttpMethod.PUT,"/api/children/{childId}").hasAnyRole("dziecko")
-				.antMatchers(HttpMethod.POST,"/api/accounts/","/api/authenticate","/api/children","/api/matches","/api/results","/api/languages","/api/statistics/statisticResults").permitAll()
-				.antMatchers(HttpMethod.POST,"/api/audio","/api/categories","/api/words","/api/categories/teachers","/api/games","/api/images","/api/statistics","/api/words").hasAnyRole("administrator","nauczyciel")
-				.antMatchers(HttpMethod.POST,"/api/roles","/api/teachers").hasRole("administrator")
-				.antMatchers(HttpMethod.DELETE,"/api/accounts/{accountId}","/api/words/{audioId}","/api/audio/{audioId}"
-						,"/api/categories/{categoryId}","/api/games/{gameId}","/api/matches/{gameMatchId}","/api/results","/api/images/{imageId}","/api/languages/{languageId}","/api/roles","/api/statistics/statisticResults/{statisticResultsId}","/api/statistics/{statisticId}","/api/teachers/{teacherId}","/api/words/{wordId}").hasRole("administrator")
-				.antMatchers(HttpMethod.DELETE,"/api/children/{childId}").hasRole("dziecko")
 				.anyRequest().authenticated()
 				.and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
