c9484f13bb5a68b26566fb22bd936da1506c1778
Adam Zimny <adam.zimny@antologic.com>
2023-07-20 15:50:30 +0200

    Add authentication filter

--- src/main/java/engineeringthesis/androidrestapi/security/domain/JwtAuthenticationFilter.java
+++ src/main/java/engineeringthesis/androidrestapi/security/domain/JwtAuthenticationFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,7 +16,6 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,48 +24,48 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 @RequiredArgsConstructor
 class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private final JwtUtils jwtUtils;
+    private final JwtUtils jwtUtils;
 
-  @Override
-  protected void doFilterInternal(
-    final @NonNull HttpServletRequest request,
-    final @NonNull HttpServletResponse response,
-    final @NonNull FilterChain filterChain
-  ) throws ServletException, IOException {
-    final String authHeader = request.getHeader("Authorization");
-    final String jwt;
-    final String login;
+    @Override
+    protected void doFilterInternal(
+            final @NonNull HttpServletRequest request,
+            final @NonNull HttpServletResponse response,
+            final @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String login;
 
-    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      filterChain.doFilter(request, response);
-      return;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        login = jwtUtils.extractLogin(jwt);
+        if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            final Claims claims = jwtUtils.extractClaims(jwt);
+            final String userLogin = claims.get(JwtUtils.USER_LOGIN, String.class);
+            final String userType = claims.get(JwtUtils.USER_TYPE, String.class);
+            final String customerUuidString = claims.get(JwtUtils.CUSTOMER_UUID, String.class);
+            final String userUuidString = claims.get(JwtUtils.USER_UUID, String.class);
+
+            final UUID customerUuid = isNotBlank(customerUuidString) ? UUID.fromString(customerUuidString) : null;
+            final UUID userUuid = isNotBlank(userUuidString) ? UUID.fromString(userUuidString) : null;
+
+            if (!jwtUtils.isTokenExpired(jwt)) {
+                final UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(new LoggedUserData(userLogin, userUuid, customerUuid), null, List.of(new SimpleGrantedAuthority(userType)));
+
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
-
-    jwt = authHeader.substring(7);
-
-    login = jwtUtils.extractLogin(jwt);
-    if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-      final Claims claims = jwtUtils.extractClaims(jwt);
-      final String userLogin = claims.get(JwtUtils.USER_LOGIN, String.class);
-      final String userType = claims.get(JwtUtils.USER_TYPE, String.class);
-      final String customerUuidString = claims.get(JwtUtils.CUSTOMER_UUID, String.class);
-      final String userUuidString = claims.get(JwtUtils.USER_UUID, String.class);
-
-      final UUID customerUuid = isNotBlank(customerUuidString) ? UUID.fromString(customerUuidString) : null;
-      final UUID userUuid = isNotBlank(userUuidString) ? UUID.fromString(userUuidString) : null;
-
-      if (!jwtUtils.isTokenExpired(jwt)) {
-        final UsernamePasswordAuthenticationToken authenticationToken =
-          new UsernamePasswordAuthenticationToken(new LoggedUserData(userLogin, userUuid, customerUuid), null, List.of(new SimpleGrantedAuthority(userType)));
-
-        authenticationToken.setDetails(
-          new WebAuthenticationDetailsSource().buildDetails(request)
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-      }
-    }
-
-    filterChain.doFilter(request, response);
-  }
 }
