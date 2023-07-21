0dfdada7ade3d8003deb4bab3f31c4e4c64eef5e
Adam Zimny <adam.zimny@antologic.com>
2023-07-21 16:19:54 +0200

    Add dtos, and forms for creating object, change configuration of inject beans, delete generic mapper to method in service

--- src/main/java/engineeringthesis/androidrestapi/account/domain/AccountRepository.java
+++ src/main/java/engineeringthesis/androidrestapi/account/domain/AccountRepository.java
@@ -1,18 +1,15 @@
 package engineeringthesis.androidrestapi.account.domain;
 
+import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
-import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+interface AccountRepository extends UUIDAwareJpaRepository<Account, Integer> {
 
-interface AccountRepository  extends UUIDAwareJpaRepository<Account,Integer> {
-	
-	@Query("SELECT a FROM Account a WHERE a.name = :accountName")
-	Account findByAccountName(final String accountName);
-	
-	@Query("SELECT a FROM Account a WHERE YEAR(current_date) - YEAR(a.accountCreatedDate) > :accountExpiredAge")
-	List<Account> findExpiredAccounts(final Integer accountExpiredAge);
+    @Query("SELECT a FROM Account a WHERE a.name = :accountName")
+    Account findByAccountName(final String accountName);
+
+    @Query("SELECT a FROM Account a WHERE YEAR(current_date) - YEAR(a.accountCreatedDate) > :accountExpiredAge")
+    List<Account> findExpiredAccounts(final Integer accountExpiredAge);
 }
