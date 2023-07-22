54071b6ad2d7a7d500714a8b54e26ea7e6558239
Adam Zimny <adam.zimny@antologic.com>
2023-07-22 11:51:46 +0200

    Add dtos, and forms for creating object, change configuration of inject beans, delete generic mapper to method in service

--- src/main/java/engineeringthesis/androidrestapi/teacher/domain/Teacher.java
+++ src/main/java/engineeringthesis/androidrestapi/teacher/domain/Teacher.java
@@ -2,13 +2,13 @@ package engineeringthesis.androidrestapi.teacher.domain;
 
 
 import engineeringthesis.androidrestapi.common.entity.AbstractUUIDEntity;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
 
@@ -20,26 +20,26 @@ import lombok.experimental.FieldNameConstants;
 @FieldNameConstants
 class Teacher extends AbstractUUIDEntity {
 
-	@Column(name = "name")
-	private String name;
-	
-	@Column(name = "surname")
-	private String surname;
-	
-	@Column(name = "year_of_birth")
-	private Short yearOfBirth;
-	
-	@Column(name = "city")
-	private String city;
-	
-	@Column(name = "profession")
-	private String profession;
-	
-	@OneToOne
-	@JoinColumn(name = "language_id_fk")
-	private TeacherLanguage languageTeacher;
-	
-	@OneToOne
-	@JoinColumn(name = "account_id_fk")
-	private TeacherAccount accountTeacher;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "year_of_birth")
+    private Short yearOfBirth;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "profession")
+    private String profession;
+
+    @OneToOne
+    @JoinColumn(name = "language_id_fk")
+    private TeacherLanguage languageTeacher;
+
+    @OneToOne
+    @JoinColumn(name = "account_id_fk")
+    private TeacherAccount accountTeacher;
 }
