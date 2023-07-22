cc98b62e30e6d1e50113f94f3517659ffd8b581e
Adam Zimny <adam.zimny@antologic.com>
2023-07-22 10:36:36 +0200

    Add dtos, and forms for creating object, change configuration of inject beans, delete generic mapper to method in service

--- src/main/java/engineeringthesis/androidrestapi/audio/dto/AudioFileTableDto.java
+++ src/main/java/engineeringthesis/androidrestapi/audio/dto/AudioFileTableDto.java
@@ -2,10 +2,9 @@ package engineeringthesis.androidrestapi.audio.dto;
 
 import java.time.OffsetDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-public record AudioFileTableDto(String streamId, Byte[] fileStream, String audioFileTableName, Byte[] pathLocator, Byte[] parentPathLocator, String fileType, Long cachedFileSize, OffsetDateTime creationTime, OffsetDateTime lastWriteTime, OffsetDateTime lastAccessTime, Boolean isNew, Boolean isOffline, Boolean isHidden, Boolean isReadonly, Boolean isArchive, Boolean isSystem, Boolean isTemporary) {
+public record AudioFileTableDto(String streamId, Byte[] fileStream, String audioFileTableName, Byte[] pathLocator,
+                                Byte[] parentPathLocator, String fileType, Long cachedFileSize,
+                                OffsetDateTime creationTime, OffsetDateTime lastWriteTime,
+                                OffsetDateTime lastAccessTime, Boolean isNew, Boolean isOffline, Boolean isHidden,
+                                Boolean isReadonly, Boolean isArchive, Boolean isSystem, Boolean isTemporary) {
 }
