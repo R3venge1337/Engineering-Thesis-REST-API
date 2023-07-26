9d392e1556540d0f091ebb54039fc6d28d580d17
Adam Zimny <adam.zimny@antologic.com>
2023-07-26 11:13:23 +0200

    extends repository for JPA specification and add method existsByName

--- src/main/java/engineeringthesis/androidrestapi/role/domain/RoleRepository.java
+++ src/main/java/engineeringthesis/androidrestapi/role/domain/RoleRepository.java
@@ -1,8 +1,8 @@
 package engineeringthesis.androidrestapi.role.domain;
 
 import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-interface RoleRepository extends UUIDAwareJpaRepository<Role, Integer> {
-
-    Role findByName(final String name);
+interface RoleRepository extends UUIDAwareJpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+    Boolean existsByName(final String name);
 }
