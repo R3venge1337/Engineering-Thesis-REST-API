1d9e5aa803c5a1abbb7b98896da43b23b0631bbf
Adam Zimny <adam.zimny@antologic.com>
2023-07-26 11:12:09 +0200

    Change return objects in method, add final in arguments, add forms to save, update, filtering all roles

--- src/main/java/engineeringthesis/androidrestapi/role/RoleFacade.java
+++ src/main/java/engineeringthesis/androidrestapi/role/RoleFacade.java
@@ -1,23 +1,24 @@
 package engineeringthesis.androidrestapi.role;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.role.dto.CreateRoleForm;
 import engineeringthesis.androidrestapi.role.dto.RoleDto;
+import engineeringthesis.androidrestapi.role.dto.RoleFilterForm;
 import engineeringthesis.androidrestapi.role.dto.UpdateRoleForm;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface RoleFacade {
 
-    List<RoleDto> getAllRoles();
-
-    RoleDto getOneRoleByName(final String roleName);
+    PageDto<RoleDto> findAllRoles(final RoleFilterForm filterForm, final PageableRequest pageableRequest);
 
     RoleDto findRole(final UUID uuid);
 
-    RoleDto saveRole(final CreateRoleForm roleForm);
+    UuidDto saveRole(final CreateRoleForm createForm);
 
-    void updateRole(final UUID uuid, final UpdateRoleForm roleForm);
+    void updateRole(final UUID uuid, final UpdateRoleForm updateForm);
 
     void deleteRole(final UUID uuid);
 }
