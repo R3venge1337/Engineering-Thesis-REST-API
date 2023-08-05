a08b6bd2b4b0c9bd26f60f1ecdeb62f317d8ac67
Adam Zimny <adamczyk97@op.pl>
2023-08-05 18:56:54 +0200

    Change returned type, add static class for routes

--- src/main/java/engineeringthesis/androidrestapi/teacher/controller/TeacherController.java
+++ src/main/java/engineeringthesis/androidrestapi/teacher/controller/TeacherController.java
@@ -1,63 +1,77 @@
 package engineeringthesis.androidrestapi.teacher.controller;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.teacher.TeacherFacade;
-import engineeringthesis.androidrestapi.teacher.dto.CreateTeacherForm;
-import engineeringthesis.androidrestapi.teacher.dto.TeacherDto;
-import engineeringthesis.androidrestapi.teacher.dto.UpdateTeacherForm;
+import engineeringthesis.androidrestapi.teacher.dto.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
+import static engineeringthesis.androidrestapi.teacher.controller.TeacherController.Routes.ROOT;
+import static engineeringthesis.androidrestapi.teacher.controller.TeacherController.Routes.ROOT_ACCOUNTS;
+import static engineeringthesis.androidrestapi.teacher.controller.TeacherController.Routes.ROOT_UUID;
+
 @RestController
-@RequestMapping(value = "/api/teachers")
 @RequiredArgsConstructor
 class TeacherController {
 
     private final TeacherFacade teacherServiceImpl;
 
-    @GetMapping
-    List<TeacherDto> getAllTeachers() {
-        return teacherServiceImpl.getAllTeachers();
+    static final class Routes {
+        static final String ROOT = "/teachers";
+        static final String ROOT_UUID = ROOT + "/{uuid}";
+
+        static final String ROOT_ACCOUNTS = ROOT_UUID + "/accounts";
     }
 
-    @GetMapping(value = "/{uuid}")
-    TeacherDto getTeacher(@PathVariable final UUID uuid) {
+    @GetMapping(ROOT)
+    PageDto<TeacherDto> findTeachers(final TeacherFilterForm filterForm, final PageableRequest pageableRequest) {
+        return teacherServiceImpl.findTeachers(filterForm, pageableRequest);
+    }
+
+    @GetMapping(ROOT_UUID)
+    TeacherDto findTeacher(@PathVariable final UUID uuid) {
         return teacherServiceImpl.findTeacher(uuid);
 
     }
 
-    @GetMapping(params = "name")
-    List<TeacherDto> getTeacherByLanguageName(@RequestParam(required = false) final String name) {
-        return teacherServiceImpl.getTeachersByLanguageName(name);
+    @GetMapping(value = ROOT, params = "nickname")
+    List<TeacherDto> findTeachersByLanguageName(@RequestParam(required = false) final String name) {
+        return teacherServiceImpl.findTeachersByLanguageName(name);
     }
 
-    @PostMapping
-    TeacherDto saveTeacher(@RequestBody final CreateTeacherForm teacherForm) {
+    @PostMapping(ROOT)
+    @ResponseStatus(HttpStatus.CREATED)
+    UuidDto saveTeacher(@RequestBody final CreateTeacherForm teacherForm) {
         return teacherServiceImpl.saveTeacher(teacherForm);
     }
 
-    @PutMapping(value = "/{uuid}")
+    @PutMapping(ROOT_UUID)
     void updateTeacher(@PathVariable final UUID uuid, @RequestBody final UpdateTeacherForm teacherForm) {
         teacherServiceImpl.updateTeacher(uuid, teacherForm);
     }
 
-    @DeleteMapping(value = "/{uuid}")
-    void deleteTeacherById(@PathVariable final UUID uuid) {
+    @DeleteMapping(ROOT_UUID)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteTeacher(@PathVariable final UUID uuid) {
         teacherServiceImpl.deleteTeacher(uuid);
     }
 
-    @GetMapping(value = "/accounts")
-    TeacherDto getTeacherWithAccount(@RequestParam final String accountName) {
-        return teacherServiceImpl.getTeacherWithAccount(accountName);
+    @GetMapping(value = ROOT_ACCOUNTS)
+    TeacherWithAccountDto findTeacherWithAccount(@PathVariable final UUID uuid) {
+        return teacherServiceImpl.findTeacherWithAccount(uuid);
     }
 }
