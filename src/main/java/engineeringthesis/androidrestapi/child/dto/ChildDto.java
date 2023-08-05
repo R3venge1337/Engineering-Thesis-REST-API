151c38d21ea8bf8ff37ba996bbcfe4ed06f5c530
Adam Zimny <adamczyk97@op.pl>
2023-08-06 00:44:34 +0200

    Remove import

--- src/main/java/engineeringthesis/androidrestapi/child/dto/ChildDto.java
+++ src/main/java/engineeringthesis/androidrestapi/child/dto/ChildDto.java
@@ -1,8 +1,6 @@
 package engineeringthesis.androidrestapi.child.dto;
 
 
-import engineeringthesis.androidrestapi.account.dto.AccountDto;
-
 import java.util.UUID;
 
 public record ChildDto(UUID uuid, String name, String surname, Short yearOfBirth, String city) {
