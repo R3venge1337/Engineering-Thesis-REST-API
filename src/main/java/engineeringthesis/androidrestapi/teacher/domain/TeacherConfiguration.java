849c5ad5a0fe5314df02104b71c4639e9665bba6
Adam Zimny <adamczyk97@op.pl>
2023-08-05 18:55:32 +0200

    Add language and account also password encoder to Facade configuration

--- src/main/java/engineeringthesis/androidrestapi/teacher/domain/TeacherConfiguration.java
+++ src/main/java/engineeringthesis/androidrestapi/teacher/domain/TeacherConfiguration.java
@@ -3,12 +3,14 @@ package engineeringthesis.androidrestapi.teacher.domain;
 import engineeringthesis.androidrestapi.teacher.TeacherFacade;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 class TeacherConfiguration {
 
     @Bean
-    TeacherFacade teacherFacade(final TeacherRepository teacherRepository){
-        return new TeacherService(teacherRepository);
+    TeacherFacade teacherFacade(final TeacherRepository teacherRepository, final TeacherAccountRepository accountRepository,
+                                final TeacherLanguageRepository languageRepository, final TeacherRoleRepository roleRepository, final PasswordEncoder passwordEncoder) {
+        return new TeacherService(teacherRepository, accountRepository, languageRepository, roleRepository, passwordEncoder);
     }
 }
