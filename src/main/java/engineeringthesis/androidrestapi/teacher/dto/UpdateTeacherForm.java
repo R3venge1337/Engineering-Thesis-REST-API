bdb99f8375491d5e82a9448fb3dc39a3ab877008
Adam Zimny <adamczyk97@op.pl>
2023-08-05 18:54:55 +0200

    Add to create and update form language and account

--- src/main/java/engineeringthesis/androidrestapi/teacher/dto/UpdateTeacherForm.java
+++ src/main/java/engineeringthesis/androidrestapi/teacher/dto/UpdateTeacherForm.java
@@ -1,8 +1,11 @@
 package engineeringthesis.androidrestapi.teacher.dto;
 
+import engineeringthesis.androidrestapi.account.dto.UpdateAccountForm;
+import engineeringthesis.androidrestapi.language.dto.UpdateLanguageForm;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateTeacherForm(@NotBlank String name, @NotBlank String surname, @NotBlank String profession,
-                                @NotBlank String city, @NotNull Short yearOfBirth) {
+                                @NotBlank String city, @NotNull Short yearOfBirth, @NotNull UpdateAccountForm account,
+                                @NotNull UpdateLanguageForm language) {
 }
