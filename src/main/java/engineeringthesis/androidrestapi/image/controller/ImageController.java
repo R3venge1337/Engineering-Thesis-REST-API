d8237c0795a907fc0c8c5522457c241efd5f9402
Adam Zimny <adamczyk97@op.pl>
2023-08-11 17:59:16 +0200

    Add response statuses, filter form, static class for routes

--- src/main/java/engineeringthesis/androidrestapi/image/controller/ImageController.java
+++ src/main/java/engineeringthesis/androidrestapi/image/controller/ImageController.java
@@ -1,7 +1,11 @@
 package engineeringthesis.androidrestapi.image.controller;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.image.ImageFacade;
 import engineeringthesis.androidrestapi.image.dto.ImageDto;
+import engineeringthesis.androidrestapi.image.dto.ImageFilterForm;
 import engineeringthesis.androidrestapi.image.dto.UpdateImageForm;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,17 +22,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
+import static engineeringthesis.androidrestapi.image.controller.ImageController.Routes.ROOT;
+import static engineeringthesis.androidrestapi.image.controller.ImageController.Routes.ROOT_DOWNLOAD;
+import static engineeringthesis.androidrestapi.image.controller.ImageController.Routes.ROOT_UUID;
+
 @RestController
-@RequestMapping(value = "/api/images")
 @RequiredArgsConstructor
 class ImageController {
 
@@ -35,35 +42,45 @@ class ImageController {
 
     private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
 
-    @GetMapping
-    List<ImageDto> getAllImages() {
-        return imageFacade.getAllImages();
+    static final class Routes {
+        static final String ROOT = "/images";
+        static final String ROOT_UUID = ROOT + "/{uuid}";
+
+        static final String ROOT_DOWNLOAD = "/downloadFile/{fileName:.+}";
     }
 
-    @GetMapping(value = "/{uuid}")
+
+    @GetMapping(ROOT)
+    PageDto<ImageDto> findImages(@RequestBody final ImageFilterForm filterForm, final PageableRequest pageableRequest) {
+        return imageFacade.findImages(filterForm, pageableRequest);
+    }
+
+    @GetMapping(ROOT_UUID)
     ImageDto findImage(@PathVariable final UUID uuid) {
         return imageFacade.findImage(uuid);
     }
 
-    @PostMapping
-    ImageDto saveImage(@RequestParam("file") final MultipartFile file) {
+    @PostMapping(ROOT)
+    @ResponseStatus(HttpStatus.CREATED)
+    UuidDto saveImage(@RequestParam("file") final MultipartFile file) {
         return imageFacade.saveImage(file);
     }
 
-    @PutMapping(value = "/{uuid}")
+    @PutMapping(ROOT_UUID)
     void updateImage(@PathVariable final UUID uuid, @RequestBody final UpdateImageForm imageForm) {
         imageFacade.updateImage(uuid, imageForm);
     }
 
-    @DeleteMapping(value = "/{uuid}")
+    @DeleteMapping(ROOT_UUID)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteImage(@PathVariable final UUID uuid) {
         imageFacade.deleteImage(uuid);
     }
 
-    @GetMapping("/downloadFile/{fileName:.+}")
+    @GetMapping(ROOT_DOWNLOAD)
     ResponseEntity<Resource> downloadFile(@PathVariable final String fileName, final HttpServletRequest request) {
         // Load file as Resource
-        Resource resource = imageFacade.loadImageAsResource(fileName);
+        final Resource resource = imageFacade.loadImageAsResource(fileName);
 
         // Try to determine file's content type
         String contentType = null;
