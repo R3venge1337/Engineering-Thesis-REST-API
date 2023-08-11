2507531e67a268f5697eae6d871ff1332ffcd276
Adam Zimny <adamczyk97@op.pl>
2023-08-11 21:43:12 +0200

    Change signatures methods

--- src/main/java/engineeringthesis/androidrestapi/game/GameFacade.java
+++ src/main/java/engineeringthesis/androidrestapi/game/GameFacade.java
@@ -1,21 +1,24 @@
 package engineeringthesis.androidrestapi.game;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.game.dto.CreateGameForm;
 import engineeringthesis.androidrestapi.game.dto.GameDto;
+import engineeringthesis.androidrestapi.game.dto.GameFilterForm;
 import engineeringthesis.androidrestapi.game.dto.UpdateGameForm;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface GameFacade {
-	
 
-	List<GameDto> getAllGames();
-	
-	GameDto saveGame(final CreateGameForm gameForm);
+    PageDto<GameDto> findGames(final GameFilterForm filterForm, final PageableRequest pageableRequest);
 
-	GameDto findGame(final UUID uuid);
-	void updateGame(final UUID uuid, final UpdateGameForm gameForm);
-	
-	void deleteGame(final UUID uuid);
+    UuidDto saveGame(final CreateGameForm gameForm);
+
+    GameDto findGame(final UUID uuid);
+
+    void updateGame(final UUID uuid, final UpdateGameForm gameForm);
+
+    void deleteGame(final UUID uuid);
 }
