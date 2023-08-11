892cb67cf907ab0f103add1025ea2f49bab68ee7
Adam Zimny <adamczyk97@op.pl>
2023-08-11 18:12:11 +0200

    Implement facade methods

--- src/main/java/engineeringthesis/androidrestapi/image/domain/ImageService.java
+++ src/main/java/engineeringthesis/androidrestapi/image/domain/ImageService.java
@@ -1,15 +1,22 @@
 package engineeringthesis.androidrestapi.image.domain;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.PageableUtils;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.common.entity.ReaderPropertiesFile;
 import engineeringthesis.androidrestapi.common.exception.FileNotFoundException;
 import engineeringthesis.androidrestapi.common.exception.NotFoundException;
+import engineeringthesis.androidrestapi.common.validation.DtoValidator;
 import engineeringthesis.androidrestapi.image.ImageFacade;
 import engineeringthesis.androidrestapi.image.dto.ImageDto;
+import engineeringthesis.androidrestapi.image.dto.ImageFilterForm;
 import engineeringthesis.androidrestapi.image.dto.UpdateImageForm;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,7 +24,6 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -29,40 +35,39 @@ class ImageService implements ImageFacade {
     private final ImageFileTableRepository imageFileTableRepository;
 
     @Override
-    public List<ImageDto> getAllImages() {
-        return imageReposiitory.findAll().stream()
-                .map(this::mapToDto)
-                .toList();
+    public PageDto<ImageDto> findImages(final ImageFilterForm filterForm, final PageableRequest pageableRequest) {
+        DtoValidator.validate(filterForm);
+        DtoValidator.validate(pageableRequest);
+
+        final ImageSpecification imageSpecification = new ImageSpecification(filterForm);
+        final Page<ImageDto> images = imageReposiitory.findAll(imageSpecification, PageableUtils.createPageable(pageableRequest))
+                .map(this::mapToDto);
+
+        return PageableUtils.toDto(images);
     }
 
     @Override
-    public ImageDto saveImage(final MultipartFile file) {
+    public UuidDto saveImage(final MultipartFile file) {
         Properties prop;
         try {
             prop = readerPropertiesFile.readPropertiesFile("application.properties");
-            Path path = Paths.get(prop.getProperty("image_save_files_path"));
-            //System.out.println(path.resolve(file.getOriginalFilename()));
+            final Path path = Paths.get(prop.getProperty("image_save_files_path"));
             Files.write(path.resolve(file.getOriginalFilename()), file.getBytes());
 
         } catch (Exception e) {
             throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
         }
-        ImageFileTable imgId = imageFileTableRepository.findByName(file.getOriginalFilename());
-        String streamId = imgId.getStreamId();
-        System.out.println(streamId);
+        final ImageFileTable imgId = imageFileTableRepository.findByName(file.getOriginalFilename());
 
-        String fileDownloadUri = prop.getProperty("image_download_uri") + file.getOriginalFilename();
-        System.out.println(fileDownloadUri);
+        final String fileDownloadUri = prop.getProperty("image_download_uri") + file.getOriginalFilename();
 
-        //WordEntity word = wordRepository.findById(wordid);
-
-        Image imageObj = new Image();
+        final Image imageObj = new Image();
         imageObj.setImageFileTable(imgId);
         imageObj.setDownloadUri(fileDownloadUri);
         imageObj.setAccepted(false);
         imageObj.setNew(true);
 
-        return mapToDto(imageReposiitory.save(imageObj));
+        return new UuidDto(imageReposiitory.save(imageObj).getUuid());
     }
 
     public ImageDto findImage(final UUID uuid) {
