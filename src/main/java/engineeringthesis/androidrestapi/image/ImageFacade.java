aa5d953d28b303c44e0787ba59b57820bbfb64ce
Adam Zimny <adamczyk97@op.pl>
2023-08-11 17:59:46 +0200

    change signature of method and returns

--- src/main/java/engineeringthesis/androidrestapi/image/ImageFacade.java
+++ src/main/java/engineeringthesis/androidrestapi/image/ImageFacade.java
@@ -1,19 +1,22 @@
 package engineeringthesis.androidrestapi.image;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.image.dto.ImageDto;
+import engineeringthesis.androidrestapi.image.dto.ImageFilterForm;
 import engineeringthesis.androidrestapi.image.dto.UpdateImageForm;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ImageFacade {
 
 
-    List<ImageDto> getAllImages();
+    PageDto<ImageDto> findImages(final ImageFilterForm filterForm, final PageableRequest pageableRequest);
 
-    ImageDto saveImage(final MultipartFile file);
+    UuidDto saveImage(final MultipartFile file);
 
     ImageDto findImage(final UUID uuid);
 
