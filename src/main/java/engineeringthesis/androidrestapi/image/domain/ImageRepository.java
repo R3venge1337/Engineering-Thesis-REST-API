b94488e84b681256f97871493dc16db0551540c0
Adam Zimny <adamczyk97@op.pl>
2023-08-11 18:11:55 +0200

    Extend repository by Jpa specification

--- src/main/java/engineeringthesis/androidrestapi/image/domain/ImageRepository.java
+++ src/main/java/engineeringthesis/androidrestapi/image/domain/ImageRepository.java
@@ -2,10 +2,11 @@ package engineeringthesis.androidrestapi.image.domain;
 
 
 import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-interface ImageRepository extends UUIDAwareJpaRepository<Image, Integer> {
+interface ImageRepository extends UUIDAwareJpaRepository<Image, Integer>, JpaSpecificationExecutor<Image> {
 
     @Query("SELECT image FROM Image as image INNER JOIN image.imageFileTable  WHERE image.imageFileTable.imageFileTableName = :filename")
     Image findByName(@Param("filename") final String filename);
