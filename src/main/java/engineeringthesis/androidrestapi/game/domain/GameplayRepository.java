f93667d509c0f5d14e7a995fe94a5693eff0bdec
Adam Zimny <adamczyk97@op.pl>
2023-08-11 21:54:34 +0200

    Change long from int, remove data annotation

--- src/main/java/engineeringthesis/androidrestapi/game/domain/GameplayRepository.java
+++ src/main/java/engineeringthesis/androidrestapi/game/domain/GameplayRepository.java
@@ -2,6 +2,6 @@ package engineeringthesis.androidrestapi.game.domain;
 
 import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
 
-interface GameplayRepository extends UUIDAwareJpaRepository<Gameplay, Integer> {
+interface GameplayRepository extends UUIDAwareJpaRepository<Gameplay, Long> {
 
 }
