8c0e0de4ad329f06c06058c5f35e2c1217603c0e
Adam Zimny <adamczyk97@op.pl>
2023-08-11 17:41:51 +0200

    Extend repository by Specification jpa

--- src/main/java/engineeringthesis/androidrestapi/audio/domain/AudioRepository.java
+++ src/main/java/engineeringthesis/androidrestapi/audio/domain/AudioRepository.java
@@ -1,10 +1,11 @@
 package engineeringthesis.androidrestapi.audio.domain;
 
 import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-interface AudioRepository extends UUIDAwareJpaRepository<Audio, Integer> {
+interface AudioRepository extends UUIDAwareJpaRepository<Audio, Integer>, JpaSpecificationExecutor<Audio> {
 
     @Query("SELECT audio FROM Audio audio INNER JOIN audio.audioFileTable  WHERE audio.audioFileTable.audioFileTableName = :filename")
     Audio findByName(@Param("filename") final String filename);
