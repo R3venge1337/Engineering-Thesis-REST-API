b42aafbf894e355d742c0604a8963414be1035b6
Adam Zimny <adamczyk97@op.pl>
2023-08-11 21:41:48 +0200

    Add unique checking if game exist, error messages, specification

--- src/main/java/engineeringthesis/androidrestapi/game/domain/GameService.java
+++ src/main/java/engineeringthesis/androidrestapi/game/domain/GameService.java
@@ -1,53 +1,80 @@
 package engineeringthesis.androidrestapi.game.domain;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.PageableUtils;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.common.exception.NotFoundException;
+import engineeringthesis.androidrestapi.common.exception.NotUniqueException;
+import engineeringthesis.androidrestapi.common.validation.DtoValidator;
 import engineeringthesis.androidrestapi.game.GameFacade;
 import engineeringthesis.androidrestapi.game.dto.CreateGameForm;
 import engineeringthesis.androidrestapi.game.dto.GameDto;
+import engineeringthesis.androidrestapi.game.dto.GameFilterForm;
 import engineeringthesis.androidrestapi.game.dto.UpdateGameForm;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
 import java.util.UUID;
+
+import static engineeringthesis.androidrestapi.game.domain.GameService.ErrorMessages.GAME_FOUND;
+import static engineeringthesis.androidrestapi.game.domain.GameService.ErrorMessages.GAME_NOT_EXIST;
 
 @RequiredArgsConstructor
 class GameService implements GameFacade {
 
     private final GameRepository gameRepository;
 
+    static final class ErrorMessages {
+        static final String GAME_NOT_EXIST = "Game not exist";
+        static final String GAME_FOUND = "Game was found";
+    }
+
     @Override
-    public List<GameDto> getAllGames() {
-        return gameRepository.findAll().stream()
-                .map(this::mapToDto)
-                .toList();
+    public PageDto<GameDto> findGames(final GameFilterForm filterForm, final PageableRequest pageableRequest) {
+        DtoValidator.validate(filterForm);
+        DtoValidator.validate(pageableRequest);
+
+        final GameSpecification gameSpecification = new GameSpecification(filterForm);
+        final Page<GameDto> games = gameRepository.findAll(gameSpecification, PageableUtils.createPageable(pageableRequest))
+                .map(this::mapToDto);
+
+        return PageableUtils.toDto(games);
     }
 
     @Override
     public GameDto findGame(final UUID uuid) {
         return gameRepository.findByUuid(uuid)
                 .map(this::mapToDto)
-                .orElseThrow(() -> new NotFoundException(""));
+                .orElseThrow(() -> new NotFoundException(GAME_NOT_EXIST));
     }
 
     @Override
     @Transactional
-    public GameDto saveGame(final CreateGameForm gameForm) {
+    public UuidDto saveGame(final CreateGameForm createForm) {
+        DtoValidator.validate(createForm);
+        checkUnique(createForm.name());
+
 
         Game game = new Game();
-        game.setName(gameForm.name());
+        game.setName(createForm.name());
 
-        return mapToDto(gameRepository.save(game));
+        return new UuidDto(gameRepository.save(game).getUuid());
     }
 
 
     @Override
     @Transactional
-    public void updateGame(final UUID uuid, final UpdateGameForm gameForm) {
-        final Game game = gameRepository.findByUuid(uuid)
-                .orElseThrow(() -> new NotFoundException(""));
+    public void updateGame(final UUID uuid, final UpdateGameForm updateForm) {
+        DtoValidator.validate(updateForm);
 
-        game.setName(gameForm.name());
+        final Game game = gameRepository.findByUuid(uuid)
+                .orElseThrow(() -> new NotFoundException(GAME_NOT_EXIST));
+
+        checkUnique(updateForm.name(), game.getName());
+
+        game.setName(updateForm.name());
     }
 
     @Override
@@ -58,5 +85,17 @@ class GameService implements GameFacade {
 
     GameDto mapToDto(final Game game) {
         return new GameDto(game.getUuid(), game.getName());
+    }
+
+    private void checkUnique(final String formName, final String entityName) {
+        if (!formName.equals(entityName)) {
+            checkUnique(formName);
+        }
+    }
+
+    void checkUnique(final String name) {
+        if (gameRepository.existsByName(name)) {
+            throw new NotUniqueException(Game.Fields.name, GAME_FOUND);
+        }
     }
 }
