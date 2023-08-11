69b8c8a1a5210dfdfce301ec844bfef04c09bf2d
Adam Zimny <adamczyk97@op.pl>
2023-08-11 21:42:15 +0200

    Extend repository by Jpa specification

--- src/main/java/engineeringthesis/androidrestapi/game/domain/GameRepository.java
+++ src/main/java/engineeringthesis/androidrestapi/game/domain/GameRepository.java
@@ -1,9 +1,9 @@
 package engineeringthesis.androidrestapi.game.domain;
 
 import engineeringthesis.androidrestapi.common.repository.UUIDAwareJpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-interface GameRepository extends UUIDAwareJpaRepository<Game, Integer> {
-    @Query("SELECT g FROM Game g WHERE g.name = :gameName")
-    Game findByGameName(final String gameName);
+interface GameRepository extends UUIDAwareJpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+
+    Boolean existsByName(final String name);
 }
