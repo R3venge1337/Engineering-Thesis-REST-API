01647bdd173cea52c65443ac67e0e6caf9df9033
Adam Zimny <adamczyk97@op.pl>
2023-08-11 19:04:10 +0200

    Add validators, chane signatures of methods

--- src/main/java/engineeringthesis/androidrestapi/word/domain/WordService.java
+++ src/main/java/engineeringthesis/androidrestapi/word/domain/WordService.java
@@ -1,17 +1,20 @@
 package engineeringthesis.androidrestapi.word.domain;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.PageableUtils;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.common.exception.NotFoundException;
+import engineeringthesis.androidrestapi.common.validation.DtoValidator;
 import engineeringthesis.androidrestapi.word.WordFacade;
 import engineeringthesis.androidrestapi.word.dto.CreateWordForm;
 import engineeringthesis.androidrestapi.word.dto.UpdateWordForm;
 import engineeringthesis.androidrestapi.word.dto.WordDto;
+import engineeringthesis.androidrestapi.word.dto.WordFilterForm;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 
-import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -20,27 +23,39 @@ class WordService implements WordFacade {
     private final WordRepository wordRepository;
 
     @Override
-    public List<WordDto> getAllWords() {
-        return wordRepository.findAll().stream()
-                .map(this::mapToDto)
-                .toList();
+    public PageDto<WordDto> findWords(final WordFilterForm filterForm, final PageableRequest pageableRequest) {
+        DtoValidator.validate(filterForm);
+        DtoValidator.validate(pageableRequest);
+
+        final WordSpecification wordSpecification = new WordSpecification(filterForm);
+        final Page<WordDto> words = wordRepository.findAll(wordSpecification, PageableUtils.createPageable(pageableRequest))
+                .map(this::mapToDto);
+
+        return PageableUtils.toDto(words);
     }
 
     @Override
     @Transactional
-    public WordDto saveWord(final CreateWordForm wordForm) {
+    public UuidDto saveWord(final CreateWordForm wordForm) {
+        DtoValidator.validate(wordForm);
 
         final Word word = new Word();
         word.setName(wordForm.name());
         word.setDownloadUri(wordForm.downloadUri());
 
-        final Word savedEntity = wordRepository.save(word);
-        return mapToDto(savedEntity);
+        return new UuidDto(wordRepository.save(word).getUuid());
     }
 
     @Override
-    public WordDto getWordByName(final String wordName) {
-        return mapToDto(wordRepository.getWordByName(wordName));
+    @Transactional
+    public void updateWord(final UUID uuid, final UpdateWordForm wordForm) {
+        DtoValidator.validate(wordForm);
+
+        final Word word = wordRepository.findByUuid(uuid)
+                .orElseThrow(() -> new NotFoundException(""));
+
+        word.setName(wordForm.name());
+        word.setDownloadUri(wordForm.downloadUri());
     }
 
     @Override
@@ -50,33 +65,14 @@ class WordService implements WordFacade {
     }
 
     @Override
-    @Transactional
-    public WordDto updateWord(final UUID uuid, final UpdateWordForm wordForm) {
+    public PageDto<WordDto> getWordsByCategoryName(final String categoryName, final PageableRequest pageableRequest) {
+        final Page<WordDto> words = wordRepository.findAllByCategory_Name(categoryName, PageableUtils.createPageable(pageableRequest))
+                .map(this::mapToDto);
 
-        final Word word = wordRepository.findByUuid(uuid)
-                .orElseThrow(() -> new NotFoundException(""));
-
-        word.setName(wordForm.name());
-        word.setDownloadUri(wordForm.downloadUri());
-        wordRepository.save(word);
-
-        return mapToDto(word);
-    }
-
-    @Override
-    public Page<WordDto> getWordsByCategoryName(String categoryName, Integer page, Integer size) {
-        int pageNumber = page != null && page > 0 ? page : 0;
-        int sizeNumber = size != null && size > 0 ? size : 4;
-        return mapToPageDto(wordRepository.findAllByCategory_Name(categoryName, PageRequest.of(pageNumber, sizeNumber)));
-    }
-
-    private Page<WordDto> mapToPageDto(Page<Word> pages) {
-        return new PageImpl<>(pages.stream().map(this::mapToDto).toList());
+        return PageableUtils.toDto(words);
     }
 
     WordDto mapToDto(final Word word) {
         return new WordDto(word.getUuid(), word.getName(), word.getDownloadUri());
     }
-
-
 }
