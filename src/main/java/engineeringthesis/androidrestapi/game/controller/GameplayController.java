8d248cdd30d0da9a2501369be05a9f8d37c17ccc
Adam Zimny <adamczyk97@op.pl>
2023-08-12 17:17:28 +0200

    Add static class routes, response statuses

--- src/main/java/engineeringthesis/androidrestapi/game/controller/GameplayController.java
+++ src/main/java/engineeringthesis/androidrestapi/game/controller/GameplayController.java
@@ -1,51 +1,65 @@
 package engineeringthesis.androidrestapi.game.controller;
 
-import java.util.List;
-import java.util.UUID;
-
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.game.GameplayFacade;
 import engineeringthesis.androidrestapi.game.dto.CreateGameplayForm;
 import engineeringthesis.androidrestapi.game.dto.GameplayDto;
+import engineeringthesis.androidrestapi.game.dto.GameplayFilterForm;
 import engineeringthesis.androidrestapi.game.dto.UpdateGameplayForm;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+import static engineeringthesis.androidrestapi.game.controller.GameplayController.Routes.ROOT;
+import static engineeringthesis.androidrestapi.game.controller.GameplayController.Routes.ROOT_UUID;
+
 @RestController
-@RequestMapping(value = "/api/matches")
+@RequiredArgsConstructor
 class GameplayController {
 
-	@Autowired
-	private GameplayFacade gameplayFacade;
+    private final GameplayFacade gameplayFacade;
 
-	@GetMapping
-	List<GameplayDto> getAllGameplays() {
-		return gameplayFacade.getAllGameplay();
-	}
+    static final class Routes {
+        static final String ROOT = "/matches";
+        static final String ROOT_UUID = ROOT + "/{uuid}";
+    }
 
-	@GetMapping(value = "/{uuid}")
+
+    @GetMapping(ROOT)
+    PageDto<GameplayDto> findGameplays(@RequestBody final GameplayFilterForm filterForm, final PageableRequest pageableRequest) {
+        return gameplayFacade.findGameplays(filterForm, pageableRequest);
+    }
+
+    @GetMapping(ROOT_UUID)
     GameplayDto findGameplay(@PathVariable final UUID uuid) {
-		return gameplayFacade.findGameplay(uuid);
-	}
+        return gameplayFacade.findGameplay(uuid);
+    }
 
-	@PostMapping
-    GameplayDto saveGameplay(@RequestBody final CreateGameplayForm gameplayForm) {
-		return gameplayFacade.saveGameplay(gameplayForm);
-	}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    UuidDto saveGameplay(@RequestBody final CreateGameplayForm gameplayForm) {
+        return gameplayFacade.saveGameplay(gameplayForm);
+    }
 
-	@PutMapping(value = "/{uuid}")
+    @PutMapping(ROOT_UUID)
     void updateGameplay(@PathVariable final UUID uuid, @RequestBody final UpdateGameplayForm gameplayForm) {
-		gameplayFacade.updateGameplay(uuid, gameplayForm);
-	}
+        gameplayFacade.updateGameplay(uuid, gameplayForm);
+    }
 
-	@DeleteMapping(value = "/{uuid}")
-	void deleteGameplay(@PathVariable final UUID uuid) {
-		gameplayFacade.deleteGameplay(uuid);
-	}
+    @DeleteMapping(ROOT_UUID)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteGameplay(@PathVariable final UUID uuid) {
+        gameplayFacade.deleteGameplay(uuid);
+    }
 }
