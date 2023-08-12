116d17969780d7917aad870edc5c7e9c4dffb26b
Adam Zimny <adamczyk97@op.pl>
2023-08-13 00:11:57 +0200

    Implement methods from facade, add error messages

--- src/main/java/engineeringthesis/androidrestapi/statistic/domain/StatisticResultService.java
+++ src/main/java/engineeringthesis/androidrestapi/statistic/domain/StatisticResultService.java
@@ -1,15 +1,23 @@
 package engineeringthesis.androidrestapi.statistic.domain;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.PageableUtils;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.common.exception.NotFoundException;
+import engineeringthesis.androidrestapi.common.validation.DtoValidator;
 import engineeringthesis.androidrestapi.statistic.StatisticResultFacade;
 import engineeringthesis.androidrestapi.statistic.dto.CreateStatisticResultForm;
 import engineeringthesis.androidrestapi.statistic.dto.StatisticResultDto;
+import engineeringthesis.androidrestapi.statistic.dto.StatisticResultFilterForm;
 import engineeringthesis.androidrestapi.statistic.dto.UpdateStatisticResultForm;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
 import java.util.UUID;
+
+import static engineeringthesis.androidrestapi.statistic.domain.StatisticResultService.ErrorMessages.STATISTIC_RESULT_NOT_EXIST;
 
 @RequiredArgsConstructor
 class StatisticResultService implements StatisticResultFacade {
@@ -17,37 +25,50 @@ class StatisticResultService implements StatisticResultFacade {
 
     private final StatisticResultRepository statisticResultRepository;
 
+    static final class ErrorMessages {
+        static final String STATISTIC_RESULT_NOT_EXIST = "Statistic result not exist";
+        static final String STATISTIC_RESULT_FOUND = "Statistic result was found";
+    }
+
     @Override
-    public List<StatisticResultDto> getAllStatisticResults() {
-        return statisticResultRepository.findAll().stream()
-                .map(this::mapToDto)
-                .toList();
+    public PageDto<StatisticResultDto> findStatisticResults(final StatisticResultFilterForm filterForm, final PageableRequest pageableRequest) {
+        DtoValidator.validate(filterForm);
+        DtoValidator.validate(pageableRequest);
+
+        final StatisticResultSpecification specification = new StatisticResultSpecification(filterForm);
+        final Page<StatisticResultDto> results = statisticResultRepository.findAll(specification, PageableUtils.createPageable(pageableRequest))
+                .map(this::mapToDto);
+
+        return PageableUtils.toDto(results);
     }
 
     @Override
     @Transactional
-    public StatisticResultDto saveStatisticResult(final CreateStatisticResultForm resultForm) {
+    public UuidDto saveStatisticResult(final CreateStatisticResultForm createForm) {
+        DtoValidator.validate(createForm);
 
-        StatisticResult statisticResultEntity = new StatisticResult();
+        final StatisticResult statisticResultEntity = new StatisticResult();
+        statisticResultEntity.setResult(createForm.result());
 
-        return mapToDto(statisticResultRepository.save(statisticResultEntity));
+        return new UuidDto(statisticResultRepository.save(statisticResultEntity).getUuid());
     }
 
     @Override
     public StatisticResultDto findStatisticResult(final UUID uuid) {
         return statisticResultRepository.findByUuid(uuid)
                 .map(this::mapToDto)
-                .orElseThrow(() -> new NotFoundException(""));
+                .orElseThrow(() -> new NotFoundException(STATISTIC_RESULT_NOT_EXIST));
     }
 
     @Override
     @Transactional
-    public void updateStatisticResult(final UUID uuid, final UpdateStatisticResultForm resultForm) {
+    public void updateStatisticResult(final UUID uuid, final UpdateStatisticResultForm updateForm) {
+        DtoValidator.validate(updateForm);
 
-        StatisticResult statisticResult = statisticResultRepository.findByUuid(uuid)
-                .orElseThrow(() -> new NotFoundException(""));
+        final StatisticResult statisticResult = statisticResultRepository.findByUuid(uuid)
+                .orElseThrow(() -> new NotFoundException(STATISTIC_RESULT_NOT_EXIST));
 
-        statisticResult.setResult(resultForm.result());
+        statisticResult.setResult(updateForm.result());
     }
 
     @Override
--- src/main/java/engineeringthesis/androidrestapi/statistic/domain/StatisticTypeService.java
+++ src/main/java/engineeringthesis/androidrestapi/statistic/domain/StatisticTypeService.java
@@ -1,15 +1,23 @@
 package engineeringthesis.androidrestapi.statistic.domain;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.PageableUtils;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.common.exception.NotFoundException;
+import engineeringthesis.androidrestapi.common.validation.DtoValidator;
 import engineeringthesis.androidrestapi.statistic.StatisticTypeFacade;
 import engineeringthesis.androidrestapi.statistic.dto.CreateStatisticTypeForm;
+import engineeringthesis.androidrestapi.statistic.dto.StatisticFilterForm;
 import engineeringthesis.androidrestapi.statistic.dto.StatisticTypeDto;
 import engineeringthesis.androidrestapi.statistic.dto.UpdateStatisticTypeForm;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
 import java.util.UUID;
+
+import static engineeringthesis.androidrestapi.statistic.domain.StatisticTypeService.ErrorMessages.STATISTIC_NOT_EXIST;
 
 @RequiredArgsConstructor
 class StatisticTypeService implements StatisticTypeFacade {
@@ -17,37 +25,49 @@ class StatisticTypeService implements StatisticTypeFacade {
 
     private final StatisticTypeRepository statisticTypeRepository;
 
+    static final class ErrorMessages {
+        static final String STATISTIC_NOT_EXIST = "Statistic  not exist";
+    }
+
     @Override
-    public List<StatisticTypeDto> getAllStatistic() {
-        return statisticTypeRepository.findAll().stream()
-                .map(this::mapToDto)
-                .toList();
+    public PageDto<StatisticTypeDto> findStatistics(final StatisticFilterForm filterForm, final PageableRequest pageableRequest) {
+        DtoValidator.validate(filterForm);
+        DtoValidator.validate(pageableRequest);
+
+        final StatisticSpecification specification = new StatisticSpecification(filterForm);
+        final Page<StatisticTypeDto> statistics = statisticTypeRepository.findAll(specification, PageableUtils.createPageable(pageableRequest))
+                .map(this::mapToDto);
+
+        return PageableUtils.toDto(statistics);
     }
 
     @Override
     @Transactional
-    public StatisticTypeDto saveStatisticType(final CreateStatisticTypeForm statisticTypeForm) {
+    public UuidDto saveStatisticType(final CreateStatisticTypeForm createForm) {
+        DtoValidator.validate(createForm);
 
-        StatisticType statisticTypeEntity = new StatisticType();
+        StatisticType statistic = new StatisticType();
+        statistic.setName(createForm.name());
 
-        return mapToDto(statisticTypeRepository.save(statisticTypeEntity));
+        return new UuidDto(statisticTypeRepository.save(statistic).getUuid());
     }
 
     @Override
     public StatisticTypeDto findStatisticType(final UUID uuid) {
         return statisticTypeRepository.findByUuid(uuid)
                 .map(this::mapToDto)
-                .orElseThrow(() -> new NotFoundException(""));
+                .orElseThrow(() -> new NotFoundException(STATISTIC_NOT_EXIST));
     }
 
     @Override
     @Transactional
-    public void updateStatisticType(final UUID uuid, final UpdateStatisticTypeForm statisticTypeForm) {
+    public void updateStatisticType(final UUID uuid, final UpdateStatisticTypeForm updateForm) {
+        DtoValidator.validate(updateForm);
 
-        StatisticType statisticType = statisticTypeRepository.findByUuid(uuid)
-                .orElseThrow(() -> new NotFoundException(""));
+        final StatisticType statisticType = statisticTypeRepository.findByUuid(uuid)
+                .orElseThrow(() -> new NotFoundException(STATISTIC_NOT_EXIST));
 
-        statisticType.setName(statisticTypeForm.name());
+        statisticType.setName(updateForm.name());
     }
 
     @Override
