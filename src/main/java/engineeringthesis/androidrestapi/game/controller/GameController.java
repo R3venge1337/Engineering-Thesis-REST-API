b9a357ff72b02446f945a6c9a412eab0ffb914aa
Adam Zimny <adamczyk97@op.pl>
2023-08-12 17:15:47 +0200

    Remove unusued imports

--- src/main/java/engineeringthesis/androidrestapi/game/controller/GameController.java
+++ src/main/java/engineeringthesis/androidrestapi/game/controller/GameController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,6 +55,7 @@ class GameController {
     void updateGame(@PathVariable final UUID uuid, @RequestBody final UpdateGameForm gameForm) {
         gameFacade.updateGame(uuid, gameForm);
     }
+
     @DeleteMapping(ROOT_UUID)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteGame(@PathVariable final UUID uuid) {
