27152b470ca2e72567a87c4e890871de17051062
Adam Zimny <adamczyk97@op.pl>
2023-08-13 00:12:22 +0200

    Delete unused imports

--- src/main/java/engineeringthesis/androidrestapi/statistic/StatisticTypeFacade.java
+++ src/main/java/engineeringthesis/androidrestapi/statistic/StatisticTypeFacade.java
@@ -8,18 +8,17 @@ import engineeringthesis.androidrestapi.statistic.dto.StatisticFilterForm;
 import engineeringthesis.androidrestapi.statistic.dto.StatisticTypeDto;
 import engineeringthesis.androidrestapi.statistic.dto.UpdateStatisticTypeForm;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface StatisticTypeFacade {
-		
-	PageDto<StatisticTypeDto> findStatistics(final StatisticFilterForm filterForm, final PageableRequest pageableRequest);
-	
-	UuidDto saveStatisticType(final CreateStatisticTypeForm createForm);
 
-	StatisticTypeDto findStatisticType(final UUID uuid);
-	
-	void updateStatisticType(final UUID uuid, final UpdateStatisticTypeForm updateForm);
-	
-	void deleteStatistic(final UUID uuid);
+    PageDto<StatisticTypeDto> findStatistics(final StatisticFilterForm filterForm, final PageableRequest pageableRequest);
+
+    UuidDto saveStatisticType(final CreateStatisticTypeForm createForm);
+
+    StatisticTypeDto findStatisticType(final UUID uuid);
+
+    void updateStatisticType(final UUID uuid, final UpdateStatisticTypeForm updateForm);
+
+    void deleteStatistic(final UUID uuid);
 }
