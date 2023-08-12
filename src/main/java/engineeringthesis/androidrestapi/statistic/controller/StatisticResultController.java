bcc17a029dcf42ae18664b6e402d52c8f5b796db
Adam Zimny <adamczyk97@op.pl>
2023-08-13 00:10:22 +0200

    Add static class for routes, response statuses

--- src/main/java/engineeringthesis/androidrestapi/statistic/controller/StatisticResultController.java
+++ src/main/java/engineeringthesis/androidrestapi/statistic/controller/StatisticResultController.java
@@ -1,51 +1,65 @@
 package engineeringthesis.androidrestapi.statistic.controller;
 
+import engineeringthesis.androidrestapi.common.controller.PageDto;
+import engineeringthesis.androidrestapi.common.controller.PageableRequest;
+import engineeringthesis.androidrestapi.common.controller.UuidDto;
 import engineeringthesis.androidrestapi.statistic.StatisticResultFacade;
 import engineeringthesis.androidrestapi.statistic.dto.CreateStatisticResultForm;
 import engineeringthesis.androidrestapi.statistic.dto.StatisticResultDto;
+import engineeringthesis.androidrestapi.statistic.dto.StatisticResultFilterForm;
 import engineeringthesis.androidrestapi.statistic.dto.UpdateStatisticResultForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.UUID;
 
+import static engineeringthesis.androidrestapi.statistic.controller.StatisticResultController.Routes.ROOT;
+import static engineeringthesis.androidrestapi.statistic.controller.StatisticResultController.Routes.ROOT_UUID;
+
 @RestController
-@RequestMapping(value = "/api/statistics/statisticResults")
 @RequiredArgsConstructor
 class StatisticResultController {
 
-
     private final StatisticResultFacade statisticResultServiceImpl;
 
-    @GetMapping
-    List<StatisticResultDto> getAllStatisticResults() {
-        return statisticResultServiceImpl.getAllStatisticResults();
+
+    static final class Routes {
+        static final String ROOT = "/statistics/statisticResults";
+
+        static final String ROOT_UUID = ROOT + "/{uuid}";
     }
 
-    @GetMapping(value = "/{uuid}")
+    @GetMapping(ROOT)
+    PageDto<StatisticResultDto> findStatisticResults(@RequestBody final StatisticResultFilterForm filterForm, final PageableRequest pageableRequest) {
+        return statisticResultServiceImpl.findStatisticResults(filterForm, pageableRequest);
+    }
+
+    @GetMapping(ROOT_UUID)
     StatisticResultDto findStatisticResult(@PathVariable final UUID uuid) {
         return statisticResultServiceImpl.findStatisticResult(uuid);
     }
 
-    @PostMapping
-    StatisticResultDto saveStatisticResults(@RequestBody final CreateStatisticResultForm statisticResultForm) {
+    @PostMapping(ROOT)
+    @ResponseStatus(HttpStatus.CREATED)
+    UuidDto saveStatisticResults(@RequestBody final CreateStatisticResultForm statisticResultForm) {
         return statisticResultServiceImpl.saveStatisticResult(statisticResultForm);
     }
 
-    @PutMapping(value = "/{uuid}")
+    @PutMapping(ROOT_UUID)
     void updateStatisticResults(@PathVariable final UUID uuid, @RequestBody final UpdateStatisticResultForm statisticResultForm) {
         statisticResultServiceImpl.updateStatisticResult(uuid, statisticResultForm);
     }
 
-    @DeleteMapping(value = "/{uuid}")
+    @DeleteMapping(ROOT_UUID)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteStatisticResult(@PathVariable final UUID uuid) {
         statisticResultServiceImpl.deleteStatisticResults(uuid);
     }
